45d7197a71163e90965f041970a6421a03890f81
Kehinde Gabriel OLANIYI <kehindegabrielolaniyi22@gmail.com>
2024-12-07 23:21:55 +0100

    Add files via upload

--- classwork/Promise/httpreq.js
+++ classwork/Promise/httpreq.js
@@ -1,19 +1,38 @@
-const req = new XMLHttpRequest();
+// req.open("GET", "https://catfact.ninja/facts?limit=156max_length=60");
+// req.send();
+// console.log(req.response);
+// req.responseType = "json";
+// req.onload = () => console.log(req.response.fact);
 
-req.open("GET", "https://catfact.ninja/facts?limit=156max_length=60");
-req.send();
-console.log(req.response);
-req.responseType = "json";
-req.onload = () => console.log(req.response.fact);
+const fact = document.getElementById("Get-Fact");
+const displayResult = document.getElementById("result");
+const cat = new XMLHttpRequest();
 
-const getFacts = new Promise((resolve, reject) => {
-  req.open("GET", "https://catfact.ninja/facts?limit=156max_length=60");
-  req.send();
-  req.responseType = "json";
-  req.onload = () => resolve(req, "response");
-  req.onerror = () => reject(req, "statusText");
+fact.addEventListener("click", () => {
+  cat.open("GET", "https://catfact.ninja/fact");
+  cat.responseType = "json";
+  cat.onload = () => {
+    if (cat.status === 200) {
+      displayResult.textContent = cat.response.fact;
+    } else {
+      displayResult.textContent = "Failed";
+    }
+  };
+  cat.send();
 });
 
-getFacts
-  .then((result) => console.log(result))
-  .catch((error) => console.log(error));
+// fact.addEventListener("click", () => {
+//   const getFacts = new Promise((resolve, reject) => {
+//     req.open("GET", "https://catfact.ninja/facts?limit=15&max_length=60");
+//     req.send();
+//     req.responseType = "json";
+//     req.onload = () => resolve(req, "response");
+//     req.onerror = () => reject(req, "statusText");
+//   });
+
+//   getFacts
+//     .then((result) => (displayResult.textContent = result.response.data))
+//     .catch((error) => console.log(error));
+// });
+
+
